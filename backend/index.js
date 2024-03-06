6d75fcdccdd820a8ab09df22f06ac2c631336fa8
Abhishek <108791816+Abhi-shek03@users.noreply.github.com>
2024-03-06 23:42:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 const connectToMongo = require('./db');
 app.use(express.json())
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 // app.get('/', (req, res)=>{
 //     res.send('Hello Abhishek')
 // })
